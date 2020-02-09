ce40278285d24440f60cbad2560bdeb1ea4ed600
mbrenohd <mths.brn@gmail.com>
2020-02-09 16:29:11 -0300

    Completed module

--- stats/pitching.py
+++ stats/pitching.py
@@ -5,19 +5,19 @@ from data import games
 plays = games[games['type'] == 'play']
 
 # Task 2
-strikes_outs = plays[plays['event'].str.contains('K')]
+strike_outs = plays[plays['event'].str.contains('K')]
 
 # Task 3
-strikes_outs = strikes_outs.groupby(['year', 'game_id']).size()
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
 
 # Task 4
-strikes_outs = strikes_outs.reset_index(name='strike_outs')
+strike_outs = strike_outs.reset_index(name='strike_outs')
 
 # Task 5
-strikes_outs = strikes_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
 
 #Task 6
-strikes_outs.plot(x='year', y='strike_outs', kind='scatter')
+strike_outs.plot(x='year', y='strike_outs', kind='scatter')
 plt.legend('Strike Outs')
 plt.show()
 
