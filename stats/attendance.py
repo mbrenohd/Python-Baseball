21c0b38cc625289877737cc30384c80df72ca403
mbrenohd <mths.brn@gmail.com>
2020-02-09 16:25:53 -0300

    Completed module

--- stats/attendance.py
+++ stats/attendance.py
@@ -13,7 +13,8 @@ attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
 plt.xlabel('Year')
 plt.ylabel('Attendance')
 
-plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='green')
+plt.axhline(y=attendance['attendance'].mean(),
+            label='Mean', linestyle='--',
+            color='green')
 
 plt.show()
-
--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+# Task 1
+from data import games
+plays = games[games['type'] == 'play']
+
+# Task 2
+strikes_outs = plays[plays['event'].str.contains('K')]
+
+# Task 3
+strikes_outs = strikes_outs.groupby(['year', 'game_id']).size()
+
+# Task 4
+strikes_outs = strikes_outs.reset_index(name='strike_outs')
+
+# Task 5
+strikes_outs = strikes_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+
+#Task 6
+strikes_outs.plot(x='year', y='strike_outs', kind='scatter')
+plt.legend('Strike Outs')
+plt.show()
+
+
+
+
+
+
+
+
