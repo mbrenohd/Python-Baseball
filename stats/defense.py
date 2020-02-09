cde976b8952467b605a91e05ce843976d683c83a
mbrenohd <mths.brn@gmail.com>
2020-02-09 18:04:34 -0300

    Completed module

--- stats/defense.py
+++ stats/defense.py
@@ -10,10 +10,10 @@ plays = games.query("type=='play' & event != 'NP'")
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
 # Task 4
-pa = plays.iloc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
+pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
 
 # Task 5
-pa = pa.groupby(['year', 'game_id', 'team']).size().reset.index(name='PA')
+pa = pa.groupby(['year', 'game_id', 'team']).size().reset_index(name='PA')
 
 # Task 6
 events = events.set_index(['year', 'game_id', 'team', 'event_type'])
@@ -24,10 +24,10 @@ events = events.unstack().fillna(0).reset_index()
 # Task 8
 events.columns = events.columns.droplevel()
 events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
-events = events.rename_axis(label=None, axis='columns')
+events = events.rename_axis(None, axis='columns')
 
 # Task 9
-events_plus_pa = pd.merge(pa, events, set='outer', left_on=['year', 'game_id', 'team'],  right_on=['year', 'game_id', 'team'])
+events_plus_pa = pd.merge(events, pa, how='outer', left_on=['year', 'game_id', 'team'], right_on=['year', 'game_id', 'team'])
 
 # Task 10
 defense = pd.merge(events_plus_pa, info)
@@ -38,7 +38,7 @@ defense.loc[:, 'DER'] = 1 - ((defense['H'] + defense['ROE']) / (defense['PA'] - 
 defense.loc[:, 'year'] = pd.to_numeric(defense['year'])
 
 # Task 12
-der = defense.loc(defense.year >= 1978, ['year', 'defense', 'DER'])
+der = defense.loc[defense['year'] >= 1978, ['year', 'defense', 'DER']]
 der = der.pivot(index='year', columns='defense', values='DER')
 
 # Task 13
